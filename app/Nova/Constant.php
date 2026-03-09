f38679b53b9f1fb29ded24d94877b216be71c5ce
a.s.khoroshun@gmail.com <JtrUl5Zh-*T5KVOs@BiX>
2026-03-09 19:32:54 +0200

    edit constant table

--- app/Nova/Constant.php
+++ app/Nova/Constant.php
@@ -28,17 +28,41 @@ class Constant extends Resource
                 ->nullable()
                 ->searchable(),
 
-            Text::make('b')->nullable()->rules('max:255'),
-            Text::make('d')->nullable()->rules('max:255'),
+            Number::make('T, K', 'temperature')
+                ->step('any')
+                ->nullable(),
 
-            Number::make('n')->step('any')->nullable(),
-            Number::make('k')->step('any')->nullable(),
-            Number::make('m')->step('any')->nullable(),
-            Number::make('l')->step('any')->nullable(),
-            Number::make('temperature')->step('any')->nullable(),
-            Number::make('a')->step('any')->nullable(),
+            Number::make('B (MPa⁻ⁿ/h)', 'b')
+                ->step('any')
+                ->nullable(),
 
-            Textarea::make('source')->alwaysShow()->nullable(),
+            Number::make('D (MPa⁻ᵐ/h)', 'd')
+                ->step('any')
+                ->nullable(),
+
+            Number::make('n')
+                ->step('any')
+                ->nullable(),
+
+            Number::make('l')
+                ->step('any')
+                ->nullable(),
+
+            Number::make('m')
+                ->step('any')
+                ->nullable(),
+
+            Number::make('k')
+                ->step('any')
+                ->nullable(),
+
+            Number::make('a')
+                ->step('any')
+                ->nullable(),
+
+            Textarea::make('source')
+                ->alwaysShow()
+                ->nullable(),
         ];
     }
 
